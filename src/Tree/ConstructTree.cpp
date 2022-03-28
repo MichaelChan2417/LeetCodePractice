a9acd40db16498c9fec1a7aed8fc025204b1551f
MichaelChan2417 <45192104+MichaelChan2417@users.noreply.github.com>
2022-03-28 18:14:27 +0800

    Update ConstructTree.cpp

--- src/Tree/ConstructTree.cpp
+++ src/Tree/ConstructTree.cpp
@@ -46,3 +46,58 @@ TreeNode* bstFromPreorder(vector<int>& preorder) {
     return root;
 }
 
+
+// Here we use another way to complete the function
+void constructTree(TreeNode* r, vector<int>& preorder, int i, int j) {
+    // if only one left
+    int r_val = r->val;
+    int k = 0;
+
+    if (i > j) return;
+    if (i == j) {
+        TreeNode* temp_node = new TreeNode(preorder[i]);
+        if (r_val < preorder[i]) {
+            r->right = temp_node;
+        }
+        else {
+            r->left = temp_node;
+        }
+        return;
+    }
+
+    for (k = i; k <= j; k++) {
+        if (preorder[k] > r_val) {
+            break;
+        }
+    }
+
+    if (k == i) {
+        TreeNode* temp_node = new TreeNode(preorder[k]);
+        r->right = temp_node;
+        constructTree(r->right, preorder, k + 1, j);
+    }
+    else if (k == j + 1) {
+        TreeNode* temp_node = new TreeNode(preorder[i]);
+        r->left = temp_node;
+        constructTree(r->left, preorder, i + 1, j);
+        return;
+    }
+    else {
+        TreeNode* temp_node1 = new TreeNode(preorder[i]);
+        TreeNode* temp_node2 = new TreeNode(preorder[k]);
+        r->left = temp_node1;
+        r->right = temp_node2;
+        constructTree(r->left, preorder, i + 1, k - 1);
+        constructTree(r->right, preorder, k + 1, j);
+        return;
+    }
+
+};
+
+
+TreeNode* bstFromPreorder2(vector<int>& preorder) {
+    TreeNode* root = new TreeNode(preorder[0]);
+    int n = preorder.size();
+    constructTree(root, preorder, 1, n - 1);
+    return root;
+};
